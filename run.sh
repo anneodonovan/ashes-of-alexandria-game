b714ab48fe47e89fac5cb75ed85648d2603e6322
anneodonovan <anneodonovan006@gmail.com>
2026-08-14 00:08:14 +0100

    Fix run.sh classpath separator on Windows
    java -cp uses ':' as the path separator on Unix but ';' on Windows;
    hardcoding ':' broke the script when run from Git Bash on Windows.
    Detect the platform via uname and pick the right separator.

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Run the main JavaFX class with Gson + JavaFX
 
-java -cp "bin:lib/gson-2.13.2.jar:resources" \
+CP_SEP=":"
+case "$(uname -s)" in
+    MINGW*|MSYS*|CYGWIN*) CP_SEP=";" ;;
+esac
+
+java -cp "bin${CP_SEP}lib/gson-2.13.2.jar${CP_SEP}resources" \
      --module-path ~/javafx/javafx-sdk-21.0.9/lib \
      --add-modules javafx.controls,javafx.fxml \
      com.alexandria.view.GameFrame
